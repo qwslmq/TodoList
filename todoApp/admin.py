1f216da42466a4906491308ce4abd3d7f1bcc267
qwslmq <qwslmq@126.com>
2018-04-30 21:46:27 +0800

    first commit

---
+++ todoApp/admin.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.User)
+# Register your models here.
---
+++ todoApp/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+                ('password', models.CharField(max_length=256)),
+                ('email', models.EmailField(unique=True, max_length=254)),
+                ('sex', models.CharField(default=b'\xe7\x94\xb7', max_length=32, choices=[(b'male', b'\xe7\x94\xb7'), (b'female', b'\xe5\xa5\xb3')])),
+                ('c_time', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+                'ordering': ['-c_time'],
+                'verbose_name': '\u7528\u6237',
+                'verbose_name_plural': '\u7528\u6237',
+            },
+        ),
+    ]
